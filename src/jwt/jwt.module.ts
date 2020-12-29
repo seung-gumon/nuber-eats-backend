9b05dca8c9a356e67fbd2b8bb3f13688c9ae33e7
SeungGumon <b9327912@gmail.com>
2020-12-30 00:14:20 +0900

    make JWT module Part Two

--- src/jwt/jwt.module.ts
+++ src/jwt/jwt.module.ts
@@ -1,14 +1,22 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
 import { JwtService } from './jwt.service';
+import { CONFIG_OPTIONS } from './jwt.constants';
+import { JwtModuleOptions } from './jwt.interfaces';
 
 @Module({})
 @Global()
 export class JwtModule {
-  static forRoot(): DynamicModule {
+  static forRoot(options: JwtModuleOptions): DynamicModule {
     return {
       module: JwtModule,
+      providers: [
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: options,
+        },
+        JwtService,
+      ],
       exports: [JwtService],
-      providers: [JwtService],
     };
   }
 }
--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { JwtModuleOptions } from './jwt.interfaces';
+import { CONFIG_OPTIONS } from './jwt.constants';
+import { Inject, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class JwtService {}
+export class JwtService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
+  ) {
+    console.log(options);
+  }
+
+  hello() {
+    console.log('hello');
+  }
+}
