49ade4346a3a3fef8fe5a4397033e208b62dc22b
SeungGumon <b9327912@gmail.com>
2021-06-30 01:02:55 +0900

    create Upload Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,7 @@ import {CommonModule} from "./common/common.module";
 import { PaymentsModule } from './payments/payments.module';
 import {Payment} from "./payments/entities/payment.entity";
 import {ScheduleModule} from "@nestjs/schedule";
+import { UploadsModule } from './uploads/uploads.module';
 
 @Module({
     imports: [
@@ -77,7 +78,8 @@ import {ScheduleModule} from "@nestjs/schedule";
         }),
         OrdersModule,
         CommonModule,
-        PaymentsModule
+        PaymentsModule,
+        UploadsModule
     ],
     controllers: [],
     providers: [],
